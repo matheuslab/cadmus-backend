d71b8d476c12ef429550b64abb22d129a3bfd5ee
Matheus Martins <matheus.martins@ee.ufcg.edu.br>
2020-04-21 16:17:40 -0300

    changing response body

--- controllers/ChallengeController.js
+++ controllers/ChallengeController.js
@@ -1,18 +1,18 @@
 var data = {};
 
 function createMap (data) {
-    let arr = new Array(data.height);
+    let arr = new Array(data.width);
 
-    for(let index=0;index<data.height;index++){
-        arr[index]= new Array(data.width).fill('.');
+    for(let index=0;index<data.width;index++){
+        arr[index]= new Array(data.height).fill('.');
     }
 
     for(let index=0; index<data.airport; index++){
         let randomHeight = Math.trunc(Math.random() * data.height);
         let randomWidth = Math.trunc(Math.random() * data.width);
 
-        if(arr[randomHeight][randomWidth] !== 'A')
-            arr[randomHeight][randomWidth] = 'A';
+        if(arr[randomWidth][randomHeight] !== 'A')
+            arr[randomWidth][randomHeight] = 'A';
         else {
             index--;
         }
@@ -22,8 +22,8 @@ function createMap (data) {
         let randomHeight = Math.trunc(Math.random() * data.height);
         let randomWidth = Math.trunc(Math.random() * data.width);
 
-        if(arr[randomHeight][randomWidth] !== 'A' && arr[randomHeight][randomWidth] !== '*')
-            arr[randomHeight][randomWidth] = '*';
+        if(arr[randomWidth][randomHeight] !== 'A' && arr[randomWidth][randomHeight] !== '*')
+            arr[randomWidth][randomHeight] = '*';
         else {
             index--;
         }
@@ -35,19 +35,20 @@ function createMap (data) {
 function cloudGrowth (map, data) {
     let tempArr= JSON.parse(JSON.stringify( map ));
     let test = JSON.parse(JSON.stringify( map ));
+    let response = [];
     let hasfirstAirportAffected = false;
-    let dayOfFirstInfection = false;
+    let dayOfFirstInfection = 0;
     let countAirportsInfected = 0;
     let daysToTotalInfection = 0;
 
     for(let days = 2;data.airport-countAirportsInfected>0;days++){
-        for(let i=0;i<data.height;i++){
-            for(let j=0;j<data.width;j++){
+        for(let i=0;i<data.width;i++){
+            for(let j=0;j<data.height;j++){
                 if(test[i][j] === '*'){
-                    let up = i+1 < data.height ? tempArr[i+1][j]: false;
+                    let up = i+1 < data.width ? tempArr[i+1][j]: false;
                     let down = i-1 >= 0 ? tempArr[i-1][j] : false;
                     let left = j-1 >= 0 ? tempArr[i][j-1] : false;
-                    let right = j+1 < data.width ? tempArr[i][j+1]: false;
+                    let right = j+1 < data.height ? tempArr[i][j+1]: false;
 
                     if(up){
                         if(up === 'A'){
@@ -96,16 +97,17 @@ function cloudGrowth (map, data) {
             }
         }
         test = JSON.parse(JSON.stringify( tempArr ));
-        console.log(countAirportsInfected);
         if(data.airport-countAirportsInfected <= 0){
             daysToTotalInfection = days;
+
         }
+        response.push(test);
     }
 
     return {
-        ...test,
         dayOfFirstInfection,
-        daysToTotalInfection
+        daysToTotalInfection,
+        ...response
     };
 }
 
@@ -119,11 +121,11 @@ module.exports = {
 
     sendResult(request, response){
 
-        var map = createMap(data)
+        var map = createMap(data);
 
         return response.json({
-            map,
-            final: cloudGrowth(map, data),
+            initialState: map,
+            otherDays: cloudGrowth(map, data),
         });
     }
 }
